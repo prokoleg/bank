171889b0f7c959b7e9c8389ecc97a63ac69d9a53
Oleg Prokofiev <89846796+prokoleg@users.noreply.github.com>
2022-08-09 22:24:54 +0300

    Create class_db.php

---
+++ inc/classes/class_db.php
@@ -0,0 +1,24 @@
+<?php
+class Database {
+
+    public $conn;
+    public $sql;
+
+    function __construct($sql)
+    {
+        $this->sql = $sql;
+    }
+
+    // получаем соединение с БД
+    public function getConnection(){
+        
+        include_once (__DIR__.'../config.php');
+        
+        $this->conn = new mysqli(HOST, USER, PASS, DATABASE);
+
+        return $this->conn->query($this->sql);
+    }
+    public function getCliseDb(){
+        return $this->conn->close();
+    }
+}
