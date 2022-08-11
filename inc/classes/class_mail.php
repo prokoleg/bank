b2c084a6dcfd653bf90796ed7c7ad5b72e11e31f
Oleg Prokofiev <89846796+prokoleg@users.noreply.github.com>
2022-08-12 01:54:40 +0300

    Add files via upload

---
+++ inc/classes/class_mail.php
@@ -0,0 +1,29 @@
+<?php
+// script by Prokofiev Oleg info@blanet.ru ©2022
+//
+// Права на данный скрипт пренадлежает его автору.
+// Модификация и использование данного скрипта разрешается только с разрешения
+// его автора Прокофьева Олега (info@blanet.ru)
+// Незаконное использование и распространение скрипта ЗАПРЕЩЕНО
+// Скрипт распространяется по лицензии MIT
+
+class Mail
+{
+	public static $to;
+	public static $from = "info@blanet.ru";
+	private static $subject;
+        public static $message;
+	public static $login;
+
+	public static function getMail()
+	{
+		self::$message = "<h2>Здравствуйте ".self::$login."!</h2> <p>Недавно Вы регистрировались на проекте <strong>bank.blanet.ru</strong>. Для активации своей учетной записи перейдите по <a href='https://bank.blanet.ru/?verefy=".self::$to."'>ссылке</a></p><p>Если ссылка не работает, то просто скопируйте ее и вставьте в адресную строку вашего браузера: <strong>https://bank.blanet.ru/?verefy=".self::$to."</strong></p><p>Если вы не регистрировались, то просто проигнорируйте это письмо или сообщите <a href='mailto:info@blanet.ru?subject=Предупреждение%20о%20возможном%20взломе%20аккаунта&body=Я%20не%20регистрировался%20на%20вашем%20проекте.%20Проверьте%20пожалуйста%20вашего%20пользователя%20".self::$to."!'>администратору</a> Blanet.Ru</p><hr>С уважением. Команда Blanet.Ru";
+		$headers  = "Content-type: text/html; charset=UTF-8 \r\n";
+		$headers .= "From: Blanet.Ru <info@blanet.ru>\r\n";
+                $headers .= "X-Mailer: Blanet.Ru";
+
+                self::$subject = "=?UTF-8?B?" . base64_encode("Подтверждение учетной записи") . "?=";
+
+		return mail(self::$to, self::$subject, self::$message, $headers);
+	}
+}
