7bb89aa02e5c12938ed6f315dbed0ea35582dfc6
Oleg Prokofiev <89846796+prokoleg@users.noreply.github.com>
2022-08-21 14:45:27 +0300

    Create dashboard.js
    Signed-off-by: Oleg Prokofiev <89846796+prokoleg@users.noreply.github.com>

---
+++ admin/js/dashboard.js
@@ -0,0 +1,52 @@
+/* globals Chart:false, feather:false */
+(() => {
+  'use strict'
+
+  feather.replace({ 'aria-hidden': 'true' })
+
+  // Graphs
+  const ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Понедельник',
+        'Вторник',
+        'Среда',
+        'Четверг',
+        'Пятница',
+        'Суббота',
+        'Воскресенье'
+      ],
+      datasets: [{
+        data: [
+          15,
+          17,
+          18,
+          20,
+          23,
+          24,
+          20
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+})()
