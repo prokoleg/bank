b1a7fcdd918ff37053f5549e248311da4eeb4c76
Oleg Prokofiev <89846796+prokoleg@users.noreply.github.com>
2022-08-21 14:47:37 +0300

    Create graph.php
    Signed-off-by: Oleg Prokofiev <89846796+prokoleg@users.noreply.github.com>

---
+++ admin/skin/graph.php
@@ -0,0 +1,19 @@
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+  <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Dashboard</h1>
+    <div class="btn-toolbar mb-2 mb-md-0">
+      <div class="btn-group me-2">
+        <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+        <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+      </div>
+      <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
+        <span data-feather="calendar" class="align-text-bottom"></span>
+        This week
+      </button>
+    </div>
+  </div>
+
+  <canvas class="my-4 w-100" id="myChart" width="900" height="380">
+  	Ваш браузер не поддерживает тег <code>canvas</code>
+  </canvas>
+</main>
